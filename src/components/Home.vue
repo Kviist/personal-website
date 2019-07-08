cf473dded418d09e012f1a593d7ca548a93b55e8
Renos Zabounidis <rzabounidis@umass.edu>
2019-07-07 21:15:15 -0400

    changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,16 @@
 <template>
 <div>
-  <vue-mathjax :formula="formula"></vue-mathjax>
+  <VueMathjax :formula="formula"></VueMathjax>
 </div>
 </template>
 
 <script>
 
-import {VueMathjax} from 'vue-mathjax'
+import {VueMathjax} from './vue-mathjax.vue'
 export default {
-  components: {
-    VueMathjax
-  },
-  created() {
-    let ckeditor = document.createElement('script');
-    ckeditor.setAttribute('src',"https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS_HTML");
-    document.head.appendChild(ckeditor);
-},
-  data () {
-    return {
-      formula: '$x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}$',
-      msg: 'Welcome to Your Vue.js App'
+    components: {
+        VueMathjax
     }
-  }
 }
+  
 </script>
